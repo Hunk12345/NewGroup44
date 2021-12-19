219097f8eb7b2e1ac4d87408677dad48876c9ac7
RUTUJA <RUTUJA@DESKTOP-RRJEGOL>
2021-12-19 17:48:36 +0530

    added do while loop

---
+++ src/dataType/Do_while_loop.java
@@ -0,0 +1,15 @@
+package dataType;
+
+public class Do_while_loop {
+
+		public static void main(String[] args) {
+			int a=1;
+			do {
+				System.out.println(a);
+				a++;
+			}
+			while(a<=10);
+
+		}
+
+	}
